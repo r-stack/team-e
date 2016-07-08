76d2473c259d6c09326a2ae20959a60b13818502
Shunji Kawabata <shunji@kwbt.org>
2016-07-08 10:24:54 +0900

    modified models.py

--- voteme/postermaker/models.py
+++ voteme/postermaker/models.py
@@ -1,18 +1,17 @@
 # @Author: kwbt69
 # @Date:   2016-07-07T16:00:42+09:00
 # @Last modified by:   kwbt69
-# @Last modified time: 2016-07-07T18:47:13+09:00
+# @Last modified time: 2016-07-08T10:24:30+09:00
 
 
 from __future__ import unicode_literals
 
 from django.db import models
 
-class Category(models.Model):
+class Politician(models.Model):
     twitter_account = models.CharField(max_length=30)
-    keyword = models.CharField(max_length=30)
-    count = models.IntegerField()
+    manifest = models.CharField(max_length=30)
 
-class Manifest(models.Model):
-    twitter_account = models.CharField(max_length=30)
-    manifest_wording = models.CharField(max_length=30)
+class Category(models.Model):
+    politician = models.ForeignKey(Manifest)
+    keyword = models.CharField(max_length=30)
