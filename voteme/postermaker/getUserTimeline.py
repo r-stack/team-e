1ca7076edf138c69e958e454e30e13eac6fe9674
GTsuchida <g.tsuchida@gmail.com>
2016-07-08 01:05:28 +0000

    reviced getUserTimeline.py

--- voteme/postermaker/getUserTimeline.py
+++ voteme/postermaker/getUserTimeline.py
@@ -9,18 +9,24 @@ import sys
 import twitter
 import settings
 
-# 引数チェック
-argv = sys.argv
+def getUserTimeline(userName, tweetNum):
 
-# ToDo: 土田アカウントで認証処理しているので変更したい
-api = twitter.Api(consumer_key=settings.CONSUMER_KEY,
-                  consumer_secret=settings.CONSUMER_SECRET,
-                  access_token_key=settings.ACCESS_TOKEN,
-                  access_token_secret=settings.ACCESS_TOKEN_SECRET)
+    # ToDo: 土田アカウントで認証処理しているので変更したい
+    api = twitter.Api(consumer_key=settings.CONSUMER_KEY,
+                      consumer_secret=settings.CONSUMER_SECRET,
+                      access_token_key=settings.ACCESS_TOKEN,
+                      access_token_secret=settings.ACCESS_TOKEN_SECRET)
 
 
-state = api.GetUserTimeline(screen_name=argv[1],count = argv[2])
-for s in state :
-    print s.text.encode(sys.getfilesystemencoding())
-    # ToDo カテゴリ取得処理を呼び出し
-    
+    user = api.GetUser(screen_name=userName);
+    print user.screen_name
+    state = api.GetUserTimeline(screen_name=userName,count = tweetNum)
+    for s in state :
+        print s.text.encode(sys.getfilesystemencoding())
+        # ToDo カテゴリ取得処理を呼び出し
+    return(user,state);
+
+
+if  __name__ == "__main__":
+    argv = sys.argv
+    getUserTimeline(argv[1],argv[2])
