c505f1d52a92130c6e3f1acb9bd2d1e578f31b73
Shunji Kawabata <shunji@kwbt.org>
2016-07-08 10:27:51 +0900

    modified models.py again...

--- voteme/postermaker/models.py
+++ voteme/postermaker/models.py
@@ -1,7 +1,7 @@
 # @Author: kwbt69
 # @Date:   2016-07-07T16:00:42+09:00
 # @Last modified by:   kwbt69
-# @Last modified time: 2016-07-08T10:24:30+09:00
+# @Last modified time: 2016-07-08T10:27:26+09:00
 
 
 from __future__ import unicode_literals
@@ -13,5 +13,5 @@ class Politician(models.Model):
     manifest = models.CharField(max_length=30)
 
 class Category(models.Model):
-    politician = models.ForeignKey(Manifest)
+    politician = models.ForeignKey(Politician)
     keyword = models.CharField(max_length=30)
