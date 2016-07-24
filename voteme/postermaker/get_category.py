997303946ad081e6f3ec7b623021d0435c3564b9
Shunji Kawabata <shunji@kwbt.org>
2016-07-24 09:36:09 +0900

    pseudo integration for language recognition

--- voteme/postermaker/get_category.py
+++ voteme/postermaker/get_category.py
@@ -7,61 +7,52 @@ import json
 import requests
 import time
 
-from django import settings
-from get_user_timeline import TwitterTimeLine
+from django.conf import settings
 
 _DOCOMO_API_BASE_URL = settings.DOCOMO_API_BASE_URL
 _DOCOMO_API_KEY = settings.DOCOMO_API_KEY
 
 
-class Docomo(object):
-
+class CategoryExtractor(object):
     def __init__(self, docomo_api_key=_DOCOMO_API_KEY):
-        self.uri = "{}{}".format(_DOCOMO_API_BASE_URL, docomo_api_key)
+        self.uri = "{base_url}?APIKEY={api_key}".format(
+            base_url=_DOCOMO_API_BASE_URL, api_key=docomo_api_key)
         self.payload = {
-                "text": None,
-                "extflg": "0",
+                'text': None,
+                'extflg': '0',
                 }
         self.headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 
     def parse(self, query):
-        self.payload["text"] = query
+        self.payload['text'] = query
         r = requests.post(self.uri, data=self.payload, headers=self.headers)
         return json.loads(r.text)
 
+    def get_category_list(self, tweets):
+        category_list = []
 
-class categoryGetter(object):
+        for tweet in tweets:
+            # To avoid throughput overrun
+            time.sleep(0.3)
+            category = self.get_category(tweet)
+            if category is not None:
+                category_list.append(category)
 
-    def ext1stRankCategory(self, text):
-        agent = Docomo()
-        resp = agent.parse(text)
-        status = resp["status"]
-        count = resp["count"]
-        rawdata = resp["rawdata"]
+        return category_list
+
+    def get_category(self, text):
+        resp = self.parse(text)
+        count = resp['count']
 
         # Ignore a too short tweet
-        if(count == 0):
+        if count == 0:
             return None
 
         # Get the cluster of 1st rank
-        clusters = resp["clusters"]
+        clusters = resp['clusters']
         for cluster in clusters:
-            if cluster["cluster_rank"] == 1:
-                name = cluster["cluster_name"]
+            if cluster['cluster_rank'] == 1:
+                name = cluster['cluster_name']
                 break
 
         return name
-
-    def getCategoryList(self, account, getTweetNum):
-        categoryList = []
-        user, state = getUserTimeline(account, getTweetNum)
-
-        for s in state:
-            text = s.text
-            # To avoid throughput overrun
-            time.sleep(0.3)
-            category = self.ext1stRankCategory(text)
-            if(category is not None):
-                categoryList.append(category)
-
-        return categoryList
