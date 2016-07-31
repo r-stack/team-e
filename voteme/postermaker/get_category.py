9872fb412ccdea1c6c40b59ba7365cc2254fe309
Shunji Kawabata <shunji@kwbt.org>
2016-07-31 13:04:34 +0900

    fixed bug #14

--- voteme/postermaker/get_category.py
+++ voteme/postermaker/get_category.py
@@ -3,6 +3,9 @@
 docomoの言語解析API(カテゴリ分析)を叩くコード
 """
 
+from __future__ import unicode_literals
+from logging import getLogger
+
 import json
 import requests
 import time
@@ -11,6 +14,8 @@ from django.conf import settings
 
 _DOCOMO_API_BASE_URL = settings.DOCOMO_API_BASE_URL
 _DOCOMO_API_KEY = settings.DOCOMO_API_KEY
+
+logger = getLogger(__name__)
 
 
 class CategoryExtractor(object):
@@ -31,12 +36,29 @@ class CategoryExtractor(object):
     def get_category_list(self, tweets):
         category_list = []
 
+        # packing tweets upto 500 charactors to save the number of api-call
+        packed_tweet = ''
         for tweet in tweets:
-            # To avoid throughput overrun
-            time.sleep(0.3)
-            category = self.get_category(tweet)
-            if category is not None:
-                category_list.append(category)
+            # assume that one tweet <= 140 charactors
+            logger.debug("get_category: len_packed_tweet = {}".format(
+                len(packed_tweet)))
+
+            if len(packed_tweet) > 350:
+                """
+                TODOs:
+                 - currently it ommits the 'tails' of tweets, when it doesn't
+                   make up to 350 charactors.
+                """
+                # To avoid throughput overrun
+                time.sleep(0.3)
+                categories = self.get_category(packed_tweet)
+                if categories is not None:
+                    category_list.extend(categories)
+
+                packed_tweet = ''
+
+            else:
+                packed_tweet += tweet
 
         return category_list
 
@@ -49,10 +71,17 @@ class CategoryExtractor(object):
             return None
 
         # Get the cluster of 1st rank
+        categories = []
         clusters = resp['clusters']
         for cluster in clusters:
-            if cluster['cluster_rank'] == 1:
-                name = cluster['cluster_name']
-                break
+            logger.debug("get_category: cluster_name = {cluster_name}, \
+                cluster_rate = {cluster_rate}".format(
+                cluster_name=cluster['cluster_name'],
+                cluster_rate=cluster['cluster_rate']))
+            if cluster['cluster_rate'] >= 5:
+                categories.append(cluster['cluster_name'])
 
-        return name
+        logger.debug("get_category: categories = {categories}".format(
+            categories=categories))
+
+        return categories
